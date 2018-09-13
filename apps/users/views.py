6a31a8d974392d8f335391ec71740ea42ff561ad
Cenergy <673598118@qq.com>
2018-09-13 20:52:24 +0800

    12321121

--- apps/users/views.py
+++ apps/users/views.py
@@ -236,7 +236,7 @@ def BANAJAX(request):
         print(userid)
         voice_path = "./media/voice/" + userid + ".wav"
         system_type = plat.system()
-        try:
+        if True:
             if (system_type == 'Linux'):
                 from utils.voices import stt
                 voice_words = stt.XF_text(voice_path, 16000)
@@ -250,9 +250,8 @@ def BANAJAX(request):
                 "code": 200,
                 "message": "successs!!",
                 "datas": voice_words
-
             }
-        except:
+        else:
             abc = {
                 "code": 400,
                 "message": "fail!",
