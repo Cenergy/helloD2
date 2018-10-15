ccb7903878e7d34de6e36d290425e03fc5838721
Cenergy <673598118@qq.com>
2018-10-15 15:56:55 +0800

    人脸识别改动很大的版本，完成0.99！！

--- apps/utils/email_send.py
+++ apps/utils/email_send.py
@@ -38,7 +38,7 @@ def  register_send_email(email,url_strs,type_code="register"):
         email_record.save()
     if type_code == "register":
         email_body= "<h3>请点击下面的链接激活你的账号:<p>" \
-                    "<a href='"+url_strs+"/active/"+code+"'>"+url_strs+"/active/"+code+"</a></p></h3>"
+                    "<a href='https://"+url_strs+"/active/"+code+"'>https://"+url_strs+"/active/"+code+"</a></p></h3>"
         message = MIMEText(email_body, 'html', 'utf-8')
         message['From'] = Header("AIGIS网", 'utf-8')
         try:
@@ -52,7 +52,7 @@ def  register_send_email(email,url_strs,type_code="register"):
             print(e)
     elif type_code == "forget":
         email_body = "<h3>大侠，密码太多容易忘记? 点击以下链接，再战江湖！----------><p>" \
-                     "<a href='"+url_strs+"/reset/" + code + "'>"+url_strs+"/reset/" + code + "</a></p></h3>"
+                     "<a href='https://"+url_strs+"/reset/" + code + "'>https://"+url_strs+"/reset/" + code + "</a></p></h3>"
         message = MIMEText(email_body, 'html', 'utf-8')
         message['From'] = Header("AIGIS网", 'utf-8')
         try:
