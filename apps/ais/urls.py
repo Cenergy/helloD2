f217ea07f51c07677ef1c484577b9510a2fe2ad9
cenergy <673598118@qq.com>
2020-04-10 15:47:31 +0800

    add

--- apps/ais/urls.py
+++ apps/ais/urls.py
@@ -6,9 +6,10 @@ from django.conf.urls import url
 from django.views.decorators.csrf import csrf_exempt
 from ais import views
 
+# ais 的相关路由
 urlpatterns = [
     url(r'^upload/', csrf_exempt(views.SourcesUpload.as_view()), name="upload"),
-    url(r'^imgupload/', csrf_exempt(views.ImageUpload.as_view()), name="imgupload"),
     url(r'^img2words/', csrf_exempt(views.ImgtoWords.as_view()), name="img2words"),
     url(r'^img2excel/', csrf_exempt(views.ImgtoExcel.as_view()), name="img2excel"),
+    url(r'^imgupload/', csrf_exempt(views.ImageUpload.as_view()), name="imgupload"),
 ]
